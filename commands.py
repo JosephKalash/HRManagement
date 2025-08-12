ab0d41adec9cf7f57dfb44ebc4c20ba8cd71792f
Joseph Kalash <joseph.m.kalsh@gmail.com>
2025-08-12 14:10:07 +0400

    add py file

---
+++ commands.py
@@ -0,0 +1,8 @@
+
+import os
+
+# command = 'cd HRManagement.Infrastructure'
+# os.system(command)
+# command = "dotnet ef migrations add AddCommonFields -s ../HRManagement.ApI/HRManagement.API.csproj "
+command = "dotnet ef database update -s ../HRManagement.ApI/HRManagement.API.csproj "
+os.system(command)
